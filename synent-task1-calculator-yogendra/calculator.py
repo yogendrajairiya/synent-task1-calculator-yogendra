8a7681c7e29d3fa6ff13db851afdbed8b2d0cd06
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2026-05-08 15:35:06 +0530

    Add Calculator file

---
+++ synent-task1-calculator-yogendra/calculator.py
@@ -0,0 +1,25 @@
+while True:
+    try:
+        num1 = float(input ("Enter the  first number: "))
+        num2 = float(input ( "Enter second number : "))
+        op = input("Enter operation (+, -, *, / ): ")
+
+        if op == "+" :
+            print(num1 + num2)
+        elif op == "-":
+            print(num1 - num2)
+        elif op == "*" :
+            print(num1 * num2)
+        elif op == "/":
+            print(num1/ num2 if num2 != 0 else "Cannot divide by zero")
+        else:
+            print("Invalid operation")
+
+    except:
+        print("Invalid input")
+
+    again = input("Continue? (y/n): ")
+    if again != "y":
+        break
+    
+    
